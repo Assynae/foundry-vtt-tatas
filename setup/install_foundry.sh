b46d77a29977859b6416dffbf78ea5182acdb606
Assynae <none>
2020-11-19 14:15:58 +0100

    Start foundry as a system service

--- setup/install_foundry.sh
+++ setup/install_foundry.sh
@@ -5,13 +5,24 @@
 FOUNDRY_HOME=/foundry
 
 cd $FOUNDRY_HOME
-sudo mkdir foundryvtt
-sudo mkdir foundrydata
+sudo mkdir -p foundryvtt
+sudo mkdir -p foundrydata
 
 # Install the software
 cd foundryvtt
 
-read -p 'Please enter Foundry VTT server (node.js) download URL: ' FOUNDY_DOWNLOAD_URL
+read -p 'Download Foundry VTT? [y/n] ' download
 
-sudo wget -O foundryvtt.zip "$FOUNDY_DOWNLOAD_URL"
-sudo unzip foundryvtt.zip
+if [ $download -eq "y" ]; then
+	read -p 'Please enter Foundry VTT server (node.js) download URL: ' FOUNDY_DOWNLOAD_URL
+
+	echo "Downloading Foundry VTT..."
+	sudo wget -O foundryvtt.zip "$FOUNDY_DOWNLOAD_URL"
+	echo "Unzipping Foundry VTT..."
+	sudo unzip foundryvtt.zip
+fi
+
+# Create systemd service
+echo "Setting system service for Foundry VTT..."
+sudo cp ../config/systemd/foundry-vtt.service /etc/systemd/system/
+sudo systemctl daemon-reload
--- setup/run_foundry.sh
+++ setup/run_foundry.sh
@@ -1,7 +1,3 @@
 # Start running the foundry server
 
-FOUNDRY_HOME=/foundry
-
-cd $FOUNDRY_HOME/foundryvtt
-
-node resources/app/main.js --dataPath=$FOUNDRY_HOME/foundrydata
+systemctl start foundry-vtt
