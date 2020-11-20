8a0dc083a1551f8843bca4cbaf9c26a02bda2376
Assynae <none>
2020-11-20 15:56:24 +0100

    Change bash-it theme

--- setup/shell_setup.sh
+++ setup/shell_setup.sh
@@ -2,5 +2,7 @@
 # https://github.com/Bash-it/bash-it
 
 git clone --depth=1 https://github.com/Bash-it/bash-it.git ~/.bash_it
-
 ~/.bash_it/install.sh --silent
+
+# Change theme to axin
+sed -i 's/^\(export BASH_IT_THEME=\).*$/\1"axin"/' .bashrc
